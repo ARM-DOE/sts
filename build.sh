faab43f5fb08489b1a78bed4437a3141b650c563
Dan <daniel.dohnalek@pnnl.gov>
2015-08-19 09:23:52 -0700

    Directory structure now meets go standards
    - Edited build.sh to include tests.

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-export GOPATH=$PWD/lib
+export GOPATH=$PWD
 echo "--Building dependencies"
 go get gopkg.in/yaml.v2
 go get util
 echo "--Building sender"
-go build -o sender/sender sender/main.go sender/cache.go sender/bin.go sender/webserver.go sender/sender.go
+go build -o src/sender/sender src/sender/main.go src/sender/cache.go src/sender/bin.go src/sender/webserver.go src/sender/sender.go
 echo "--Building receiver"
-go build -o receiver/receiver receiver/main.go
+go build -o src/receiver/receiver src/receiver/main.go
+echo "--Testing"
+go test -cover util
+go test -cover sender
+go test -cover receiver
