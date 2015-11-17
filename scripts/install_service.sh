5647f3e7e70564e1aab1a61ff90b38d26eb5f0e8
kolasin2 <brent@anl.gov>
2015-11-17 21:17:33 +0000

    dded install_service.sh script

---
+++ scripts/install_service.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# This script installs the start STS related start/stop scripts
+# to the proper directories under CentOS.   
+#
+# Script must be installed run as root
+#
+# Usage: install_service.sh [stssend | stsreceive]
+#
+# Brent Kolasinski
+# Argonne National Laboratory
+# brent@anl.gov
+
+STSTYPE=$1
+
+if [ $STSTYPE -eq 'stssend'  ]
+  then
+    cp ./stssend /etc/init.d/
+    chkconfig --add stssend
+elif [ $STSTYPE -eq 'stsreceive' ]
+  then
+    cp ./stsreceive /etc/init.d/
+    chkconfig --add stsreceive
+else
+    echo -n "Usage: install_service.sh [stssend | stsreceive]\n"
+fi
+
