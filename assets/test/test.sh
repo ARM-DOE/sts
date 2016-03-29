42e3b6cd427ad341cae5431a826129726ce8d0fd
Sherman Beus <sherman.beus@pnnl.gov>
2016-03-29 15:28:26 -0700

    Slightly more thorough log checking.

--- assets/test/test.sh
+++ assets/test/test.sh
@@ -50,11 +50,19 @@ $cmd > /dev/null
 echo "-------------------------------------------------------------------------"
 echo "ERRORS:"
 grep "ERROR" $STS_HOME/data/log/messages/*/*
+ototal=`cut -d ":" -f 1 $STS_HOME/data/log/outgoing_to/*/*/* | sort > /tmp/foo1`
+ ouniq=`cut -d ":" -f 1 $STS_HOME/data/log/outgoing_to/*/*/* | sort | uniq > /tmp/foo2`
+itotal=`cut -d ":" -f 1 $STS_HOME/data/log/incoming_from/*/*/* | sort > /tmp/foo3`
+ iuniq=`cut -d ":" -f 1 $STS_HOME/data/log/incoming_from/*/*/* | sort | uniq > /tmp/foo4`
 echo "-------------------------------------------------------------------------"
 echo "OUT DUPLICATES:"
-total=`cut -d ":" -f 1 $STS_HOME/data/log/outgoing_to/*/*/* | sort > /tmp/foo1`
- uniq=`cut -d ":" -f 1 $STS_HOME/data/log/outgoing_to/*/*/* | sort | uniq > /tmp/foo2`
 diff /tmp/foo1 /tmp/foo2
+echo "-------------------------------------------------------------------------"
+echo "IN DUPLICATES:"
+diff /tmp/foo3 /tmp/foo4
+echo "-------------------------------------------------------------------------"
+echo "IN vs OUT:"
+diff /tmp/foo2 /tmp/foo4
 rm /tmp/foo*
 echo "-------------------------------------------------------------------------"
 echo "OUT DIR:"
