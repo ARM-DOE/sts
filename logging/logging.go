ec54a7e1254d5f4870e57cf7ed7c17481ef0372b
Sherman Beus <sherman.beus@pnnl.gov>
2016-04-06 15:00:22 -0700

    Separate logger for each mode + prefix (host)

--- logging/logging.go
+++ logging/logging.go
@@ -2,10 +2,12 @@ package logging
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
@@ -27,14 +29,14 @@ const Msg = "messages"
 var gLoggers map[string]*Logger
 
 // Init will create a logger for each input "mode".
-func Init(modes []string, basePath string, debug bool) {
+func Init(modes []string, root string, debug bool) {
 	if gLoggers == nil {
 		gLoggers = make(map[string]*Logger)
 	}
 	for _, mode := range modes {
 		_, exists := gLoggers[mode]
 		if !exists {
-			gLoggers[mode] = newLogger(basePath, mode, debug)
+			gLoggers[mode] = newLogger(filepath.Join(root, mode), mode, debug)
 		}
 	}
 }
@@ -49,153 +51,154 @@ func Done() {
 	}
 }
 
-// GetLogger returns the logger for the specified mode.
-func GetLogger(mode string) *Logger {
-	return gLoggers[mode]
+func getLogger(mode string, prefix ...string) (*Logger, bool) {
+	if len(prefix) > 0 {
+		key := mode + ":" + strings.Join(prefix, "-")
+		if l, ok := gLoggers[key]; ok {
+			return l, true
+		}
+		if l, ok := gLoggers[mode]; ok {
+			gLoggers[key] = newLogger(filepath.Join(l.root, filepath.Join(prefix...)), l.mode, l.debug)
+			return gLoggers[key], true
+		}
+		return nil, false
+	}
+	l, ok := gLoggers[mode]
+	return l, ok
 }
 
 // Debug joins each item in params{} by a space, prepends a timestamp and the location of the message,
 // and writes the result to stdout and to the "messages" log file.
 func Debug(params ...interface{}) {
-	logger, exists := gLoggers[Msg]
+	logger, exists := getLogger(Msg)
 	if !exists || !logger.debug {
 		return
 	}
-	logger.lock.Lock()
-	defer logger.lock.Unlock()
 	_, file, line, _ := runtime.Caller(1) // Get file name and line number of the caller of this function
 	params = append([]interface{}{fmt.Sprintf("%s:%d", filepath.Base(file), line)}, params...)
-	fmt.Fprintln(os.Stdout, params...)
-	logger.rotate()
-	logger.logger.Println(params...)
+	logger.out(os.Stdout, params...)
+	logger.log(params...)
 }
 
 // Error joins each item in params{} by a space, prepends a timestamp and the location of the error,
 // and writes the result to stderr and to the "messages" log file.
 func Error(params ...interface{}) {
-	logger, exists := gLoggers[Msg]
+	logger, exists := getLogger(Msg)
 	if !exists {
 		fmt.Fprintln(os.Stderr, params...) // Prevent error message from being lost.
 		return
 	}
-	logger.lock.Lock()
-	defer logger.lock.Unlock()
 	_, file, line, _ := runtime.Caller(1) // Get file name and line number of the caller of this function
 	params = append([]interface{}{fmt.Sprintf("ERROR %s:%d", filepath.Base(file), line)}, params...)
-	fmt.Fprintln(os.Stderr, params...)
-	logger.rotate()
-	logger.logger.Println(params...)
+	logger.out(os.Stderr, params...)
+	logger.log(params...)
 }
 
 // Disked writes a formatted string to the "to_disk" log file.
 func Disked(msg string, hash string, size int64, prefix ...string) {
-	logger, exists := gLoggers[Disk]
+	logger, exists := getLogger(Disk, prefix...)
 	if !exists {
 		return
 	}
-	logger.lock.Lock()
-	defer logger.lock.Unlock()
-	logger.rotate(prefix...)
-	line := fmt.Sprintf("%s:%s:%d:%d", msg, hash, size, time.Now().Unix())
-	logger.logger.Printf(line)
-	logger.lock.Lock()
+	logger.log(fmt.Sprintf("%s:%s:%d:%d", msg, hash, size, time.Now().Unix()))
 }
 
 // Sent writes a formatted string to the "outgoing_to" log file.
 func Sent(msg string, hash string, size int64, ms int64, prefix ...string) {
-	logger, exists := gLoggers[Out]
+	logger, exists := getLogger(Out, prefix...)
 	if !exists {
 		return
 	}
-	logger.lock.Lock()
-	defer logger.lock.Unlock()
-	logger.rotate(prefix...)
-	line := fmt.Sprintf("%s:%s:%d:%d: %d ms", msg, hash, size, time.Now().Unix(), ms)
-	logger.logger.Printf(line)
-	logger.fh.Sync()
+	logger.log(fmt.Sprintf("%s:%s:%d:%d: %d ms", msg, hash, size, time.Now().Unix(), ms))
 }
 
 // FindSent looks for a file name in the outgoing log history.
 func FindSent(text string, start time.Time, stop time.Time, prefix ...string) bool {
-	return GetLogger(Out).search(text+":", start, stop, prefix...)
+	if logger, ok := getLogger(Out, prefix...); ok {
+		return logger.search(text+":", start, stop)
+	}
+	return false
 }
 
 // Received writes a formatted string to the "incoming_from" log file.
 func Received(msg string, hash string, size int64, prefix ...string) {
-	logger, exists := gLoggers[In]
+	logger, exists := getLogger(In, prefix...)
 	if !exists {
 		return
 	}
-	logger.lock.Lock()
-	defer logger.lock.Unlock()
-	logger.rotate(prefix...)
-	line := fmt.Sprintf("%s:%s:%d:%d:", msg, hash, size, time.Now().Unix())
-	logger.logger.Println(line)
-	logger.fh.Sync()
+	logger.log(fmt.Sprintf("%s:%s:%d:%d:", msg, hash, size, time.Now().Unix()))
 }
 
 // FindReceived looks for a file name in the incoming log history.
 func FindReceived(text string, start time.Time, stop time.Time, prefix ...string) bool {
-	return GetLogger(In).search(text+":", start, stop, prefix...)
-}
-
-func getPath(basePath string, mode string, time time.Time, prefix ...string) string {
-	root := filepath.Join(basePath, mode)
-	if len(prefix) > 0 {
-		root = filepath.Join(root, filepath.Join(prefix...))
+	if logger, ok := getLogger(In, prefix...); ok {
+		return logger.search(text+":", start, stop)
 	}
-	return filepath.Join(root, getYear(time)+getMonth(time), getDay(time))
+	return false
 }
 
 // Logger is the main struct for managing a rolling log file.
 type Logger struct {
-	logger   *log.Logger
-	fh       *os.File
-	lock     sync.RWMutex
-	path     string
-	basePath string
-	mode     string
-	debug    bool
+	mode   string
+	logger *log.Logger
+	fh     *os.File
+	lock   sync.RWMutex
+	root   string
+	path   string
+	debug  bool
 }
 
-func newLogger(basePath string, mode string, debug bool) *Logger {
-	logger := Logger{}
-	logger.lock = sync.RWMutex{}
-	logger.basePath = basePath
+func newLogger(root, mode string, debug bool) *Logger {
+	logger := &Logger{}
 	logger.mode = mode
+	logger.lock = sync.RWMutex{}
+	logger.root = root
 	logger.debug = debug
 	if mode == Msg {
-		if basePath != "" {
+		if root != "" {
 			logger.logger = log.New(nil, "", log.Ldate|log.Ltime)
 		}
 	} else {
 		logger.logger = log.New(nil, "", 0)
 	}
-	return &logger
+	return logger
 }
 
-func (logger *Logger) getPath(time time.Time, prefix ...string) string {
-	return getPath(logger.basePath, logger.mode, time, prefix...)
+func (logger *Logger) getPath(time time.Time) string {
+	return filepath.Join(logger.root, getYear(time)+getMonth(time), getDay(time))
 }
 
-func (logger *Logger) getCurrPath(prefix ...string) string {
-	now := time.Now()
-	return logger.getPath(now, prefix...)
+func (logger *Logger) getCurrPath() string {
+	return logger.getPath(time.Now())
 }
 
-func (logger *Logger) rotate(prefix ...string) {
-	currPath := logger.getCurrPath(prefix...)
-	_, err := os.Stat(currPath)
-	if logger.path != currPath || os.IsNotExist(err) || logger.fh == nil {
+func (logger *Logger) rotate() {
+	p := logger.getCurrPath()
+	_, err := os.Stat(p)
+	if logger.path != p || os.IsNotExist(err) || logger.fh == nil {
 		logger.close()
-		logger.path = currPath
-		os.MkdirAll(filepath.Dir(currPath), os.ModePerm)
-		logger.fh, err = os.OpenFile(currPath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+		logger.path = p
+		os.MkdirAll(filepath.Dir(p), os.ModePerm)
+		logger.fh, err = os.OpenFile(p, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 		if err != nil {
 			panic(fmt.Sprintf("Failed to open log file: %s", err.Error()))
 		}
 		logger.logger.SetOutput(logger.fh)
 	}
+}
+
+func (logger *Logger) out(writer io.Writer, t ...interface{}) {
+	logger.lock.Lock()
+	defer logger.lock.Unlock()
+	fmt.Fprintln(writer, t...)
+}
+
+func (logger *Logger) log(t ...interface{}) {
+	logger.lock.Lock()
+	defer logger.lock.Unlock()
+	logger.rotate()
+	logger.logger.Println(t...)
+	logger.fh.Sync()
 }
 
 func (logger *Logger) close() {
@@ -207,7 +210,7 @@ func (logger *Logger) close() {
 // search will look for a given text pattern in the log history.
 // If "start" is nonzero it will search from start to current.
 // If "start" is zero it will search from current back nDays.
-func (logger *Logger) search(text string, start time.Time, stop time.Time, prefix ...string) bool {
+func (logger *Logger) search(text string, start time.Time, stop time.Time) bool {
 	logger.lock.RLock()
 	defer logger.lock.RUnlock()
 	if start.IsZero() {
@@ -225,7 +228,7 @@ func (logger *Logger) search(text string, start time.Time, stop time.Time, prefi
 	}
 	b := []byte(text)
 	for {
-		path := logger.getPath(start, prefix...)
+		path := logger.getPath(start)
 		if fileutils.FindLine(path, b) != "" {
 			return true
 		}
