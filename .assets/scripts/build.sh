8c556cf312ebc7137aa16aac75f0c7b43063654f
Sherman Beus <sherman.beus@pnnl.gov>
2017-08-17 11:41:12 -0700

    Need to support APR's $GIT_TAG to get the version right since the tag won't exist until after the build

--- .assets/scripts/build.sh
+++ .assets/scripts/build.sh
@@ -7,7 +7,7 @@ fi
 basedir=$(dirname $0)
 
 if [ -f $GOPATH/bin/sts ]; then
-    echo "-- Cleaning old build"
+    echo "-- Cleaning Old Build"
     rm $GOPATH/bin/sts
     rm -rf $GOPATH/pkg
 fi
@@ -21,12 +21,15 @@ go install github.com/alecthomas/units
 go install github.com/davecgh/go-spew/spew
 
 date=`date -u '+%Y-%m-%d %H:%M:%S'`
-vers=`git describe --tags 2>/dev/null`
+vers=$GIT_TAG  # Set by APR
 if [ -z "$vers" ]; then
-    vers="v0.0.0"
+    vers=`git describe --tags 2>/dev/null`
+    if [ -z "$vers" ]; then
+        vers="v0.0.0"
+    fi
 fi
 
-echo "-- Building executable"
+echo "-- Building Executable"
 go build -o $GOPATH/bin/sts $race \
     -ldflags="-X 'main.BuildTime=$date UTC' -X 'main.Version=$vers'" \
     $GOPATH/src/code.arm.gov/dataflow/sts/cmd/main.go
