3e9c6b8bad718979c2ee7a92ecc906948091d591
Sherman Beus <sherman.beus@pnnl.gov>
2017-11-23 21:20:59 -0800

    Added memory use logging

--- .assets/test/test4.sh
+++ .assets/test/test4.sh
@@ -4,8 +4,8 @@ basedir=$(dirname $0)
 export STS_HOME=/var/tmp/sts4
 
 bin="$GOPATH/bin/sts"
-cmd_server="$bin $args --debug --mode=in"
-cmd_client="$bin $args --debug --mode=out"
+cmd_server="$bin $args --mode=in"
+cmd_client="$bin $args --mode=out"
 
 echo "Cleaning last run..."
 rm -rf $STS_HOME
@@ -34,6 +34,10 @@ for cmd in "${cmds[@]}"; do
     pids+=($!)
 done
 
+ncmd=${#cmds[@]}
+pid_server=${pids[(($ncmd-2))]}
+pid_client=${pids[(($ncmd-1))]}
+
 function ctrl_c() {
     echo "Caught signal ..."
     for pid in "${pids[@]}"; do
@@ -42,23 +46,42 @@ function ctrl_c() {
     done
     echo "Cleaning out incoming directory ..."
     find $STS_HOME/data/in -type f -exec rm {} \;
-    bash done.sh
     exit 0
 }
 
 function clean() {
     while true; do
-        sleep 30
+        sleep 10
 
         # Remove "in" files older than a minute
         find $STS_HOME/data/in -type f -mmin +1 -exec rm {} \;
+
+        # Keep the log files from getting out of hand
+        # NOTE: This actually doesn't work as long as STS keeps a file handle
+        # to the current log file(s).  Nice try, though.
+        # for log in `find $STS_HOME/data/log -type f`; do
+        #     n=`wc -l < $log`
+        #     if (( "$n" > 20000 )); then
+        #         # Remove the first million lines
+        #         sed -i -e "1,10000d" $log
+        #     fi
+        # done
     done
+}
+
+function mem() {
+    ps=`ps xv -p $1 | tail -1`
+    echo " $ps" | tr -s ' ' | cut -d ' ' -f 9
 }
 
 function check() {
     hosts=( "stsout-1" "stsout-2" )
     types=( "xs" "sm" "md" "lg" "xl" )
     while true; do
+        # Log memory usage
+        mem $pid_server >> $STS_HOME/mem.server.log
+        mem $pid_client >> $STS_HOME/mem.client.log
+
         sleep 60
 
         # Check for errors
@@ -66,6 +89,7 @@ function check() {
         if [ "$errors" ]; then
             cat <(echo "$errors")
             ctrl_c
+            return
         fi
 
         # Check for stuck "out" files
@@ -74,6 +98,7 @@ function check() {
             echo "Found old files in outgoing tree:"
             cat <(echo "$old")
             ctrl_c
+            return
         fi
 
         # Check for out-of-order delivery
@@ -86,6 +111,7 @@ function check() {
                     echo "$host:$type: out-of-order:"
                     cat <(echo "$match")
                     ctrl_c
+                    return
                 fi
             done
         done
